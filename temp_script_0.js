51b0704caef96b1314ee5cc0e6d53f10c6204eb2
Dzikri Aka Tezki <tomoriid62@gmail.com>
2026-05-20 12:38:04 +0700

    chore: hapus file temporary script

--- temp_script_0.js
+++
@@ -1,4 +0,0 @@
-
-        const adminToken = localStorage.getItem('bps_admin_token');
-        if (!adminToken) window.location.replace('/error?code=403');
-    
--- temp_script_4.js
+++
@@ -1,8 +0,0 @@
-
-        document.addEventListener('DOMContentLoaded', () => {
-            // Show floating train reminder if there are pending changes on load
-            if (isDataChanged) { 
-                showTrainBanner();
-            }
-        });
-    
